714b4fc4b7d18f799778d422ce08f6e0b5522803
Xantiros <xantiros@gmail.com>
2018-06-22 14:35:03 +0200

    Learning

---
+++ XamarinApp2/XamarinApp2/GreetPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GreetPage : ContentPage
+	{
+		public GreetPage ()
+		{
+			InitializeComponent ();
+
+            /*
+            Content = new Label
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Text = "Hello Darknes My old friend."
+
+            };
+            */
+		}
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            label.Text = String.Format("Value is {0:F2}", e.NewValue);
+        }
+    }
+}
