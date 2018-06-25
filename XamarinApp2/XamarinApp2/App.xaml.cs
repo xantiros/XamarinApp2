887d014539e0512231f19b333618cc922e60b749
Xantiros <xantiros@gmail.com>
2018-06-25 14:25:59 +0200

    AbsoluteLayout Excercise

--- XamarinApp2/XamarinApp2/App.xaml.cs
+++ XamarinApp2/XamarinApp2/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XamarinApp2
 		{
 			InitializeComponent();
 
-			MainPage = new GridPageExcercise2();
+			MainPage = new AbsolutePageExercise();
 		}
 
 		protected override void OnStart ()
