6bf7a24a537e699fdfacd44d26ade6b98e1eaaab
Xantiros <xantiros@gmail.com>
2018-06-25 11:55:51 +0200

    GridPage Excercise

--- XamarinApp2/XamarinApp2/App.xaml.cs
+++ XamarinApp2/XamarinApp2/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XamarinApp2
 		{
 			InitializeComponent();
 
-			MainPage = new StackPageExcercise2();
+			MainPage = new GridPageExcercise();
 		}
 
 		protected override void OnStart ()
