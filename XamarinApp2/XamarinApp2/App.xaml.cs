509288ac613bbf9769af380d8abe1fcaa8710f56
Xantiros <xantiros@gmail.com>
2018-06-25 16:37:11 +0200

    ImagePage 038

--- XamarinApp2/XamarinApp2/App.xaml.cs
+++ XamarinApp2/XamarinApp2/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XamarinApp2
 		{
 			InitializeComponent();
 
-			MainPage = new RelativePageExcercise();
+			MainPage = new ImagePage();
 		}
 
 		protected override void OnStart ()
---
+++ XamarinApp2/XamarinApp2/ImagePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ImagePage : ContentPage
+	{
+		public ImagePage ()
+		{
+			InitializeComponent ();
+
+            //var imageSource = (UriImageSource) ImageSource.FromUri(new Uri("https://picsum.photos/1920/1080/?random/"));
+
+            var imageSource = new UriImageSource { Uri = new Uri("https://picsum.photos/1920/1080/?random/") };
+            imageSource.CachingEnabled = false;
+            //imageSource.CacheValidity = TimeSpan.FromHours(1);
+            image.Source = imageSource;
+
+        }
+	}
+}
