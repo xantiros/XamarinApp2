e26696e877d745a1e0db7484a71b554d1b700928
Xantiros <xantiros@gmail.com>
2018-06-29 13:37:19 +0200

    040

---
+++ XamarinApp2/XamarinApp2/MarkapExtensions/EmbeddedImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp2.MarkapExtensions
+{
+    [ContentProperty("ResourceId")]
+    public class EmbeddedImage : IMarkupExtension
+    {
+        public string ResourceId { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (String.IsNullOrWhiteSpace(ResourceId))
+                return null;
+           return ImageSource.FromResource(ResourceId);
+        }
+    }
+}
