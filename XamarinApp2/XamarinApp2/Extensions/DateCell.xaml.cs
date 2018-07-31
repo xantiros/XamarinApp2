c8919e1896b52f5bfc7d0c4f51d81632328fea15
Xantiros <xantiros@gmail.com>
2018-07-31 15:17:10 +0200

    078

---
+++ XamarinApp2/XamarinApp2/Extensions/DateCell.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp2.Extensions
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DateCell : ViewCell
+	{
+        public static readonly BindableProperty labelProperty 
+            = BindableProperty.Create("Label", typeof(string), typeof(DateCell));
+
+        public string Label
+        {
+            get { return (string)GetValue(labelProperty); }
+            set { SetValue(labelProperty, value); }
+        }
+
+        public DateCell ()
+		{
+			InitializeComponent ();
+
+            BindingContext = this;
+		}
+	}
+}
